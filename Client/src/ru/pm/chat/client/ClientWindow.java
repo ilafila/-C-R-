187643b5e1e8d9098fb01b862c589502d77e0ebd
Ilia Filatov <43479669+ilafila@users.noreply.github.com>
2019-05-27 21:51:38 +0300

    Update ClientWindow.java

--- Client/src/ru/pm/chat/client/ClientWindow.java
+++ Client/src/ru/pm/chat/client/ClientWindow.java
@@ -3,5 +3,8 @@ package ru.pm.chat.client;
 
 
 public class ClientWindow extends JFrame implements ActionListener, TCPconnectionListener {
+  
+  private static final String ipAddrr = "172.20.197.223";
+    private static final int pOrt = 8189;
 
 }
